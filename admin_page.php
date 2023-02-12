0a85e5ea4c6a0b23b13a8bdbb5b7b94334fe4234
AnanasikA <valievaanastasia98@gmail.com>
2023-02-12 22:13:51 +0100

    +

--- admin_page.php
+++ admin_page.php
@@ -15,21 +15,27 @@ if(!isset($admin_id)){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>admin page</title>
-    
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>dashboard</title>
+
+   
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-    <!-- custom admin css file link  -->
-    <link rel="stylesheet" href="./css/admin_style.css">
+   <!-- custom admin css file link  -->
+   <link rel="stylesheet" href=".\css\style_admin.css">
 
 </head>
 <body>
    
 <?php @include 'admin_header.php'; ?>
-    
+
+
+
+
+<script src="./js/admin_script.js"></script>
+
 </body>
 </html>
--- login.php
+++ login.php
@@ -11,30 +11,34 @@ if(isset($_POST['submit'])){
    $filter_pass = filter_var($_POST['pass'], FILTER_SANITIZE_STRING);
    $pass = mysqli_real_escape_string($conn, md5($filter_pass));
 
-   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email'") or die('query failed');
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
 
    if(mysqli_num_rows($select_users) > 0){
- 
+      
       $row = mysqli_fetch_assoc($select_users);
 
-      if($row['user_type'] == 'admin') {
+      if($row['user_type'] == 'admin'){
 
          $_SESSION['admin_name'] = $row['name'];
          $_SESSION['admin_email'] = $row['email'];
          $_SESSION['admin_id'] = $row['id'];
          header('location:admin_page.php');
 
-    } elseif($row['user_type'] == 'user') {
+      }elseif($row['user_type'] == 'user'){
 
-        $_SESSION['user_name'] = $row['name'];
-        $_SESSION['user_email'] = $row['email'];
-        $_SESSION['user_id'] = $row['id'];
-        header('location:home.php');
-    }
+         $_SESSION['user_name'] = $row['name'];
+         $_SESSION['user_email'] = $row['email'];
+         $_SESSION['user_id'] = $row['id'];
+         header('location:home.php');
 
-}else {
-   $message[] = 'incorrect email or password!';
-}
+      }else{
+         $message[] = 'no user found!';
+      }
+
+   }else{
+      $message[] = 'incorrect email or password!';
+   }
 
 }
 
@@ -43,17 +47,17 @@ if(isset($_POST['submit'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login</title>
 
-    <!--font awesome cdn link-->
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="./css/style.css">
 
-    <!--custom css file link-->
-    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
 
@@ -69,7 +73,7 @@ if(isset($message)){
    }
 }
 ?>
-
+   
 <section class="form-container">
 
    <form action="" method="post">
@@ -81,6 +85,6 @@ if(isset($message)){
    </form>
 
 </section>
-    
+
 </body>
 </html>
