cdeffb99a13d2e8bbc01839671e0ead0d6754244
AnanasikA <valievaanastasia98@gmail.com>
2023-02-27 00:50:53 +0100

    ok

--- home.php
+++ home.php
@@ -27,7 +27,7 @@ if(!isset($user_id)){
 </head>
 <body>
 
-<?php @include 'header.php';?>
+<?php @include 'header.php'; ?>
 
 <section class="home">
 
@@ -52,8 +52,18 @@ if(!isset($user_id)){
             if(mysqli_num_rows($select_products) > 0) {
                 while($fetch_products = mysqli_fetch_assoc($select_products)) {
         ?>
-        <form action="" method="post" class="box">
-            <a href="view_page.php?pid=<php echo $fetch_products['id']; >"></a>
+        <form action="" method="POST" class="box">
+            <a href="view_page.php?pid=<?php echo $fetch_products['id']; ?>" class="fas fa-eye"></a>
+            <div class="price">$ <?php echo $fetch_products['price']; ?> /-</div>
+            <img src="./uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
+            <div class="name"><?php echo $fetch_products['name']; ?></div>
+            <input type="number" name="product_quantity" value="1" min="0" id="" class="qty">
+            <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
+            <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+            <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+            <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+            <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
+            <input type="submit" value="add to card" name="add_to_card" class="btn">
         </form>
         <?php
           }
