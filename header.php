9dd9890a504f319e942b5117f59cb0b822b237a1
AnanasikA <valievaanastasia98@gmail.com>
2023-02-27 16:14:00 +0100

    ok

--- header.php
+++ header.php
@@ -1,4 +1,18 @@
 <?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+    header('location:login.php');
+}
+
+?>
+
+<?php
 if(isset($message)){
    foreach($message as $message){
       echo '
@@ -34,7 +48,6 @@ if(isset($message)){
                         <li><a href="register.php">register</a></li>
                     </ul>
                 </li>
-
             </ul>
         </nav>
 
@@ -42,12 +55,12 @@ if(isset($message)){
             <div id="menu-btn" class="fas fa-bars"></div>
             <a href="search_page.php" class="fas fa-search"></a>
             <div id="user-btn" class="fas fa-user"></div>
-            <?php 
+            <?php
                 $select_wishlist_count = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE user_id = '$user_id'") or die('query failed');
                 $wishlist_num_rows = mysqli_num_rows($select_wishlist_count);
             ?>
             <a href="wishlist.php"><i class="fas fa-heart"></i><span>(<?php echo $wishlist_num_rows; ?>)</span></a>
-            <?php 
+            <?php
                 $select_cart_count = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
                 $cart_num_rows = mysqli_num_rows($select_cart_count);
             ?>
